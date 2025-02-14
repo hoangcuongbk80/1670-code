71cece660a8cfe9dbfbc246d0cee28f13ebbb8ac
Cuong Hoang~ <hoangcuongbk80@gmail.com>
2025-02-14 11:48:42 +0700

    initial version

---
+++ WebApplication2/Controllers/JobsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class JobsController : Controller
+    {
+        private static List<Job> jobs = new List<Job>
+        {
+            new Job { Id = 1, Title = "Software Engineer", Description = "Develop web applications", 
+                      Company = "FPT", Location = "Hanoi", PostedDate = DateTime.Now },
+            new Job { Id = 2, Title = "Data Analyst", Description = "Analyze business data", 
+                      Company = "FPT", Location = "Da Nang", PostedDate = DateTime.Now }
+        };
+
+        public IActionResult Index()
+        {
+            return View(jobs);
+        }
+    }
+}
